6aa6125e69ed1937dc7e402052e07d81233c6b13
Andres Madrid Ucha <andmadri@f0r3s25.codam.nl>
2026-03-22 17:42:58 +0100

    Fragment and Vector Shaders

--- Makefile
+++ Makefile
@@ -1,14 +1,15 @@
 NAME = my_opengl_project_executable
-SRC = $(wildcard src/*.cpp)
-CXX = clang++
+SRC = $(wildcard src/*.cpp) $(wildcard src/*.c)
+CXX = clang
 CXXFLAGS = -Wall -Werror -Wextra
+INCLUDES = -Iglad/include
 LIBS = -lglfw -lGL -ldl
 RM = rm -rf
 
 all: $(NAME)
 
 $(NAME) : $(SRC)
-	$(CLANG) $(CC) $(SRC) $(LIBS) -o $(NAME)
+	$(CXX) $(CXXFLAGS) $(SRC) $(INCLUDES) $(LIBS) -o $(NAME)
 
 clean: 
 	$(RM) $(NAME)
--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,82 @@
 #define GLFW_INCLUDE_NONE
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <glad/gl.h>
 #include <cstdio>
 
 void error_callback(int error, const char* description)
 {
-    fprintf(stderr, "GLFW Error [%d]: %s\n", error, description);
+	fprintf(stderr, "GLFW Error [%d]: %s\n", error, description);
 }
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+	(void)scancode; // Unused parameter
+	(void)mods;     // Unused parameter
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
 
 int main()
 {
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit())
-        return 1;
+	glfwSetErrorCallback(error_callback);
+	if (!glfwInit())
+		return 1;
 
-    // Window creation
-    GLFWwindow* window = glfwCreateWindow(640, 480, "My OpenGL Project", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return 1;
-    }
-    // OpenGL knows to use this window
-    glfwMakeContextCurrent(window);
-    
-    // Load OpenGL with GLAD
-    if (!gladLoadGL(glfwGetProcAddress))
-    {
-        fprintf(stderr, "Failed to initialize GLAD\n");
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        return 1;
-    }
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required for macOS
+#endif
+	// Window creation
+	GLFWwindow* window = glfwCreateWindow(1000, 1250, "My OpenGL Project", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return 1;
+	}
+	// OpenGL knows to use this window, creating a window is not enough!
+	glfwMakeContextCurrent(window);
 
-    // Call back for escape button to close the window
-    glfwSetKeyCallback(window, key_callback);
+	// Load OpenGL with GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		fprintf(stderr, "Failed to initialize GLAD\n");
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return 1;
+	}
 
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
-    glViewport(0, 0, width, height);
+	// Call back for escape button to close the window
+	glfwSetKeyCallback(window, key_callback);
 
-    glClearColor(0.02f, 0.13f, 0.17f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
 
-    while(!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
-        glfwSwapBuffers(window);
-    }
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glClearColor(0.247f, 1.0f, 0.294f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+	}
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    return 0;
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 }
